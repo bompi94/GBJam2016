0aa8ee6673ce242fbf3c14577d8e4868248a2d5c
Alessandro Buonpane <Alessandro Buonpane>
2016-10-02 23:06:25 +0200

    run jump anim orfeo

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
     public int speed;
-    public int jumpforce; 
+    public int jumpforce;
+    Animator anim; 
 
 	// Use this for initialization
 	void Start () {
+        anim = GetComponent<Animator>(); 
         //Physics2D.IgnoreCollision(GetComponent<Collider2D>(), GameObject.Find("eurydice").GetComponent<Collider2D>()); 
 	}
 	
@@ -19,11 +21,17 @@ public class Movement : MonoBehaviour {
         }
         if (Input.GetAxis("Horizontal") < 0)
         {
+            anim.SetBool("walking",true); 
             transform.localScale = new Vector3(-1,transform.localScale.y); 
         }
         else if (Input.GetAxis("Horizontal") > 0)
         {
+            anim.SetBool("walking", true);
             transform.localScale = new Vector3(1, transform.localScale.y);
+        }
+        else
+        {
+            anim.SetBool("walking", false);
         }
         transform.position += new Vector3(Input.GetAxis("Horizontal"),0,0) * Time.deltaTime * speed;
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
@@ -41,6 +49,7 @@ public class Movement : MonoBehaviour {
 
     public void Jump()
     {
+        anim.SetTrigger("jump"); 
         GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
     }
 }
