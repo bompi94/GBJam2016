112683f380104f856ef7b24bcf55e064e661e802
Alessandro Buonpane <Alessandro Buonpane>
2016-10-03 09:49:22 +0200

    give hand

--- Assets/Scripts/EurydiceScript.cs
+++ Assets/Scripts/EurydiceScript.cs
@@ -4,22 +4,55 @@ using System.Collections;
 public class EurydiceScript : MonoBehaviour {
     GameObject orfeo;
     public float speed;
-    public bool inStopZone; 
+    public bool inStopZone;
+    public bool following;
+    public SpringJoint2D spring;
+    bool seeing;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	    orfeo = GameObject.Find("orfeo");
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (!inStopZone)
+        if(orfeo.transform.position.x < transform.position.x)
         {
-            Vector3 dir = orfeo.transform.position - transform.position;
-            dir.y = 0;
-            transform.position += dir.normalized * Time.deltaTime * speed;
+            transform.localScale = new Vector3(-1, transform.localScale.y); 
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, transform.localScale.y);
+        }
+        if (!inStopZone && following)
+        {
+            spring.connectedAnchor = orfeo.transform.position;
+            //viene verso di me
+            if (orfeo.transform.localScale.x != transform.localScale.x)
+            {
+                spring.enabled = false; 
+            }
+
+            else
+            {
+                if(Vector3.Distance(transform.position,orfeo.transform.position)>=spring.distance)
+                spring.enabled = true; 
+            }
         }
 	}
+
+    public void GiveHand()
+    {
+        following = true; 
+        spring = GetComponent<SpringJoint2D>(); 
+        spring.enabled = true; 
+    }
+
+    public void LeaveHand()
+    {
+        following = false;
+        spring.enabled = false;
+    }
 
     public void Up(Vector3 pos)
     {
@@ -28,7 +61,6 @@ public class EurydiceScript : MonoBehaviour {
 
     public void SeeYou(float time)
     {
-
         Color temp = GetComponent<SpriteRenderer>().color;
         temp.a *= (1 + time);
         temp.a = Mathf.Clamp(temp.a, 0,1);  
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,28 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
     public int speed;
     public int jumpforce;
-    Animator anim; 
+    Animator anim;
+    public int handRange = 1;
+    public bool pickedUp = false;
 
-	// Use this for initialization
-	void Start () {
-        anim = GetComponent<Animator>(); 
-        //Physics2D.IgnoreCollision(GetComponent<Collider2D>(), GameObject.Find("eurydice").GetComponent<Collider2D>()); 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Jump"))
-        {
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), GameObject.Find("euridice").GetComponent<Collider2D>());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
             Jump();
-        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            PickUpEurydice();
+
         if (Input.GetAxis("Horizontal") < 0)
         {
-            anim.SetBool("walking",true); 
-            transform.localScale = new Vector3(-1,transform.localScale.y); 
+            anim.SetBool("walking", true);
+            transform.localScale = new Vector3(-1, transform.localScale.y);
         }
         else if (Input.GetAxis("Horizontal") > 0)
         {
@@ -33,23 +39,36 @@ public class Movement : MonoBehaviour {
         {
             anim.SetBool("walking", false);
         }
-        transform.position += new Vector3(Input.GetAxis("Horizontal"),0,0) * Time.deltaTime * speed;
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-            PickUpEurydice(); 
-	}
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * Time.deltaTime * speed;
+
+       
+
+    }
 
     public void PickUpEurydice()
     {
-        GameObject eurydice = GameObject.Find("eurydice");
-        Vector3 eurypos = eurydice.transform.position;
-        if (Mathf.Abs(eurypos.x-transform.position.x)<3 && Mathf.Abs(eurypos.y - transform.position.y) < 20 
-            && Mathf.Abs(eurypos.y - transform.position.y)>0)
-            eurydice.GetComponent<EurydiceScript>().Up(transform.position); 
+        GameObject eurydice = GameObject.Find("euridice");
+        if (!pickedUp)
+        {
+
+            if (Vector3.Distance(transform.position, eurydice.transform.position) <= handRange)
+            {
+                //change sprite 4 orfeo
+                eurydice.GetComponent<EurydiceScript>().GiveHand();
+                pickedUp = true;
+            }
+        }
+        else
+        {
+            eurydice.GetComponent<EurydiceScript>().LeaveHand();
+            pickedUp = false;
+        }
     }
 
     public void Jump()
     {
-        anim.SetTrigger("jump"); 
+        anim.SetTrigger("jump");
         GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
     }
 }
