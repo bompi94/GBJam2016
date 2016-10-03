541edc6db87f4cd380cf2d085bd661b1a33d493d
Alessandro <Alessandro@PC-ALEX>
2016-10-03 23:11:37 +0200

    note spawn improved

--- Assets/NoteSpawner.cs
+++ Assets/NoteSpawner.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class NoteSpawner : MonoBehaviour {
 
     public GameObject note1;
-    public GameObject note2; 
+    public GameObject note2;
+    public GameObject[] positions; 
 
 	// Use this for initialization
 	void Start () {
@@ -13,16 +14,22 @@ public class NoteSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            SpawnNote1();
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+            SpawnNote2();
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+            SpawnRandomNote(); 
     }
 
     public void SpawnNote1()
     {
-        Instantiate(note1,transform.position,Quaternion.identity); 
+        Instantiate(note1,positions[Random.Range(0,positions.Length-1)].transform.position,Quaternion.identity); 
     }
 
     public void SpawnNote2()
     {
-        Instantiate(note2, transform.position, Quaternion.identity);
+        Instantiate(note2, positions[Random.Range(0, positions.Length - 1)].transform.position, Quaternion.identity);
     }
 
     public void SpawnRandomNote()
