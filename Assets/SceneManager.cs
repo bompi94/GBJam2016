ce22d78f4c1c2680b44459f4954a2f61dbde0302
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2016-10-03 12:56:52 +0200

    Scene Transition Manager

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SceneManager : MonoBehaviour {
+
+	public GameObject[] scenes;
+
+	public int currentScene=0;
+
+	public GameObject orfeo;
+	public GameObject eurice;
+	public Vector3 delta;
+	public float speed = 5;
+	public bool changing = false;
+	// Use this for initialization
+	void Start () {
+		RepositionateCharacters ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void RepositionateCharacters(){
+		Transform entrypoint = scenes [currentScene].GetComponentInChildren<EntryScript> ().transform;
+		orfeo.transform.position = entrypoint.position;
+		eurice.transform.position = entrypoint.position + delta;
+	}
+
+	public void OnSceneChange(){
+		if (!changing) {
+			changing = true;
+			currentScene++;
+			RepositionateCharacters ();
+			StartCoroutine (CameraSliding (currentScene, () => {
+				Debug.Log ("Transition Completed");
+				changing=false;
+			}));
+		}
+	}
+
+	Vector3 GetCameraCenter(int sceneIndex){
+		return scenes [sceneIndex].GetComponentInChildren<CameraCenterScript> ().transform.position;
+	}
+
+	IEnumerator CameraSliding(int toSceneindex, Action callback){
+		Vector3 destination = GetCameraCenter (toSceneindex);
+		Debug.Log ("Going to " + destination);
+		while(Vector3.Distance(Camera.main.transform.position,destination)>0.1f){
+			//Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, destination, Time.deltaTime * speed);
+			Camera.main.transform.position = Vector3.MoveTowards (Camera.main.transform.position, destination, Time.deltaTime * speed);
+			yield return new WaitForEndOfFrame ();
+		}
+		Camera.main.transform.position = destination;
+		callback.Invoke ();
+	}
+}
---
+++ Assets/TestInputManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestInputManager : MonoBehaviour {
+	public SceneManager scnMng;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.G)) {
+			scnMng.OnSceneChange ();
+		}
+	}
+}
