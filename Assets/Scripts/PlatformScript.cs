7b001fcd4a1c8b1ff874c7c26f89b6364e587f4c
Alessandro <Alessandro@PC-ALEX>
2016-10-04 19:24:07 +0200

    added ade scene

--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -3,12 +3,10 @@ using System.Collections;
 
 public class PlatformScript : MonoBehaviour
 {
-
-
     void OnCollisionEnter2D(Collision2D coll)
     {
 
-        if (coll.gameObject.name == "orfeo" && coll.gameObject.transform.position.y>transform.position.y)
+        if (coll.gameObject.name == "orfeo" && coll.gameObject.transform.position.y > transform.position.y)
             coll.gameObject.GetComponent<Movement>().Grounded(); 
     }
 }
