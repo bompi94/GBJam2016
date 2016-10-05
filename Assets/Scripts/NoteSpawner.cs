68c26e96848cdfaaf84b782b58e91e9d8dc0f6d0
Alessandro <Alessandro@PC-ALEX>
2016-10-05 16:50:44 +0200

    first version with sounds

--- Assets/Scripts/NoteSpawner.cs
+++ Assets/Scripts/NoteSpawner.cs
@@ -5,11 +5,16 @@ public class NoteSpawner : MonoBehaviour {
 
     public GameObject note1;
     public GameObject note2;
-    public GameObject[] positions; 
+    public GameObject[] positions;
+    public AudioClip[] note1sounds;
+    public AudioClip[] note2sounds;
+    public AudioSource audioSourcenote1;
+    public AudioSource audioSourcenote2;
+    ArrayList audioQueue = new ArrayList();
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(AudioCoroutine()); 
 	}
 	
 	// Update is called once per frame
@@ -19,17 +24,35 @@ public class NoteSpawner : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Keypad2))
             SpawnNote2();
         if (Input.GetKeyDown(KeyCode.Keypad3))
-            SpawnRandomNote(); 
+            SpawnRandomNote();
+    }
+
+    IEnumerator AudioCoroutine()
+    {
+        while (true)
+        {
+            if(audioQueue.Count!=0 && !audioSourcenote1.isPlaying)
+            {
+                audioSourcenote1.clip = (AudioClip) audioQueue[0];
+                audioQueue.RemoveAt(0);
+
+                AudioSource.PlayClipAtPoint(audioSourcenote1.clip, Camera.main.transform.position); 
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 
     public void SpawnNote1()
     {
-        Instantiate(note1,positions[Random.Range(0,positions.Length-1)].transform.position,Quaternion.identity); 
+        Instantiate(note1,positions[Random.Range(0,positions.Length-1)].transform.position,Quaternion.identity);
+        audioQueue.Add(note1sounds[Random.Range(0, note1sounds.Length - 1)]);
     }
 
     public void SpawnNote2()
     {
         Instantiate(note2, positions[Random.Range(0, positions.Length - 1)].transform.position, Quaternion.identity);
+        audioQueue.Add( note2sounds[Random.Range(0, note2sounds.Length - 1)]);
     }
 
     public void SpawnRandomNote()
