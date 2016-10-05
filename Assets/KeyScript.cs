37a909894079bec63cccf89bcbf9a181cdc7e31b
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2016-10-05 16:26:48 +0200

    Door/Key work, wall work

---
+++ Assets/KeyScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyScript : MonoBehaviour {
+
+	public string keyName;
+
+	GameState state;
+
+	void Start(){
+		state = GameObject.Find ("GameState").GetComponent<GameState> ();
+		if (state.keysTaken.Contains (keyName)) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.tag == "Player" && !state.keysOwned.Contains(keyName)) {
+			state.keysOwned.Add (keyName);
+			state.keysTaken.Add (keyName);
+			Destroy (gameObject);
+		}
+	}
+}
