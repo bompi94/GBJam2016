4d20e29f4311bb93854d532fa08dce96d35897d3
Alessandro <Alessandro@PC-ALEX>
2016-10-06 15:00:20 +0200

    working on 4th level

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject orfeo;
+    public int[] specialRooms;
+    bool follow;
+    ArrayList sr; 
+
+    void Start()
+    {
+        sr = new ArrayList(specialRooms);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (follow)
+        {
+            orfeo = GameObject.Find("orfeo");
+            transform.position = new Vector3(orfeo.transform.position.x, orfeo.transform.position.y, transform.position.z);
+        }
+	}
+
+    public void LoadedLevel(int level)
+    {
+        if (sr.Contains(level))
+            follow = true;
+        else
+            follow = false; 
+
+    }
+}
