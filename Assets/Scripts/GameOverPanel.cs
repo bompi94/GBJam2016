788615dc78307855a206f8ebe028f89d7d2627ce
Alessandro <Alessandro@PC-ALEX>
2016-10-06 16:54:14 +0200

    gameover state updated

---
+++ Assets/Scripts/GameOverPanel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Activate()
+    {
+        GetComponent<Image>().enabled = true;
+        GetComponentInChildren<Text>().enabled = true;
+        StartCoroutine(WaitAndDeactivate());
+    }
+
+    IEnumerator WaitAndDeactivate()
+    {
+        yield return new WaitForSeconds(3);
+        Deactivate(); 
+    }
+
+    public void Deactivate()
+    {
+        GetComponent<Image>().enabled = false;
+        GetComponentInChildren<Text>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/LavaScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.name == "orfeo" || coll.gameObject.name == "euridice")
+        {
+            GameObject.Find("Health").GetComponent<Health>().Die(); 
+        }
+    }
+}
