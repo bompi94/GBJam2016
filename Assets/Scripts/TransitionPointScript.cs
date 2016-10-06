45d3c86b3aab5af7d71292838398d2755314b762
Alessandro <Alessandro@PC-ALEX>
2016-10-06 13:59:48 +0200

    fixed exit point

--- Assets/Scripts/TransitionPointScript.cs
+++ Assets/Scripts/TransitionPointScript.cs
@@ -1,42 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TransitionPointScript : MonoBehaviour {
+public class TransitionPointScript : MonoBehaviour
+{
 
-	public int goToScene;
-	public bool goingBack;
+    public int goToScene;
+    public bool goingBack;
 
-	public bool playerExited = true;
+    public bool playerExited = true;
+    bool inside;
 
-	LevelScript level;
+    LevelScript level;
+    GameObject orfeo; 
 
-	void Start(){
-		level = GetComponentInParent<LevelScript> ();
-	}
+    void Start()
+    {
+        level = GetComponentInParent<LevelScript>();
+    }
+
+    void Update()
+    {
+        if (orfeo != null && inside)
+        {
+            if (orfeo.GetComponent<Movement>().pickedUp && playerExited)
+            {
+                //scnManager.SceneChange (goToScene,goingBack);
+                SceneManager.ChangeScene(goToScene, goingBack);
+            }
+            if (!orfeo.GetComponent<Movement>().pickedUp && playerExited)
+            {
+                Debug.Log("Prendi Euridice coglione!");
+            }
+        }
+    }
 
 
-	void OnTriggerEnter2D(Collider2D coll){
-		if (coll.name == "orfeo") {
-			if (coll.GetComponent<Movement> ().pickedUp && playerExited) {
-				//scnManager.SceneChange (goToScene,goingBack);
-				SceneManager.UnityStronzo ();
-				SceneManager.ChangeScene (goToScene, goingBack);
-			}
-			if (!coll.GetComponent<Movement> ().pickedUp && playerExited) {
-				Debug.Log ("Prendi Euridice coglione!");
-			}
-		}
-	}
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.name == "orfeo")
+        {
+            orfeo = coll.gameObject; 
+            inside = true;
+           
+        }
+    }
 
-	void OnTriggerExit2D(Collider2D coll){
-		if (!playerExited) {
-			playerExited = true;
-			Debug.Log ("Trigger Reactived");
-		}
-	}
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.name == "orfeo")
 
-	public void DeactivateUntilExit(){
-		//disattivo il trigger finchè il player non si sposta
-		playerExited=false;
-	}
+            inside = false;
+        if (!playerExited)
+        {
+            playerExited = true;
+            Debug.Log("Trigger Reactived");
+        }
+    }
+
+    public void DeactivateUntilExit()
+    {
+        //disattivo il trigger finchè il player non si sposta
+        playerExited = false;
+    }
 }
