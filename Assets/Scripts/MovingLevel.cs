307e592c8befac9fec759be89134bbcb0c095eff
Alessandro <Alessandro@PC-ALEX>
2016-10-07 21:00:17 +0200

    working on bivio

--- Assets/Scripts/MovingLevel.cs
+++ Assets/Scripts/MovingLevel.cs
@@ -10,6 +10,6 @@ public class MovingLevel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += Vector3.right * Time.deltaTime * speed; 
+        transform.position += Vector3.left * Time.deltaTime * speed; 
 	}
 }
