afcce4b0feaf2c82a08bfde6047b96df7a2e1c9b
Alessandro <Alessandro@PC-ALEX>
2016-10-09 13:38:04 +0200

    kok

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -6,14 +6,17 @@ public class Sun : MonoBehaviour
 {
 
     float timer = 0;
-    float fadetime = 2;
+    float fadetime = 16;
+    float whiteTime = 4;
     public float speed;
     bool active = false;
     GameObject s;
+    GameObject t; 
     // Use this for initialization
     void Start()
     {
         s = GameObject.Find("SUN");
+        t = GameObject.Find("FINALTEXT"); 
         s.GetComponent<Image>().enabled = true;
         Destroy(GameObject.Find("orfeo"));
         Destroy(GameObject.Find("euridice"));
@@ -23,10 +26,21 @@ public class Sun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GameObject.Find("GameState").GetComponent<AudioSource>().volume -= speed * Time.deltaTime;
-        Color sun = s.GetComponent<Image>().color;
-        sun.a -= ((speed/1.5f) * Time.deltaTime);
-        s.GetComponent<Image>().color = sun;
+        timer += Time.deltaTime;
+        if (timer >= fadetime)
+        {
+            
+            Color c = t.GetComponent<Text>().color;
+            c.a += ((speed) * Time.deltaTime);
+            t.GetComponent<Text>().color = c;
+        }
+        if (timer >= whiteTime)
+        {
+            GameObject.Find("GameState").GetComponent<AudioSource>().volume -= speed * Time.deltaTime;
+            Color sun = s.GetComponent<Image>().color;
+            sun.a -= ((speed / 1.5f) * Time.deltaTime);
+            s.GetComponent<Image>().color = sun;
+        }
     }
 
 }
