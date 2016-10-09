66f35233fd6d717c3cc6acf2c80314f06903f4cb
Alessandro <Alessandro@PC-ALEX>
2016-10-09 14:13:29 +0200

    ifbhog

---
+++ Assets/Scripts/coboldopoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coboldopoint : MonoBehaviour {
+
+    public GameObject coboldo; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.name == "orfeo")
+            Instantiate(coboldo, transform.position, Quaternion.identity); 
+    }
+}
